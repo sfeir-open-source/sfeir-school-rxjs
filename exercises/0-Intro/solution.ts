8d36d3371c6b457809232235696842fbe147d719
Wolfgang Goedel <wgoedel@gmail.com>
2019-12-08 13:13:52 +0100

    exos(intro): name it

--- exercises/0-Intro/solution.ts
+++ exercises/0-Intro/solution.ts
@@ -4,22 +4,25 @@ import {
   accumulateFuelForMass
 } from '../lib';
 
+const not = <T, U>(f: (v: T) => boolean) => (x: T) => !f(x);
+const add = (a: number, b: number) => a + b;
+
 export function getModuleMasses(filename: string) {
   return readInputFile(filename)
     .split('\n')
     .map(m => parseInt(m, 10))
-    .filter(x => !isNaN(x));
+    .filter(not(isNaN));
 }
 
 export function getRequiredFuel(masses: number[]) {
   return masses
     .map(getRequiredFuelForMass)
     .filter(m => m > 0)
-    .reduce((a, b) => a + b);
+    .reduce(add);
 }
 
 export function getTotalRequiredFuel(masses: number[]) {
   return masses
     .flatMap(m => [...accumulateFuelForMass(m)])
-    .reduce((a, b) => a + b);
+    .reduce(add);
 }
