5b5085b6e5416147c3ca4a165725908b572f8763
Morgane TROYSI <troysi.m@sfeir.com>
2020-09-01 11:09:33 +0200

    Add ref on messages container

--- exercises/react-chat/src/Messages.jsx
+++ exercises/react-chat/src/Messages.jsx
@@ -1,17 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import Message from './Message'
 import './Messages.css'
 
 const Messages = ({ messages, username }) => {
+  const container = useRef(null)
 
   useEffect(() => {
-    const container = document.getElementById('messages-container')
-    if (container) container.scrollTop = container.scrollHeight
+    if (container.current) container.current.scrollTop = container.current.scrollHeight
   }, [messages])
 
   return (
-    <div id="messages-container" className="messages-container">
+    <div id="messages-container" className="messages-container" ref={container}>
       {messages.map((message, index) =>
         <Message key={`${username}_${index}`} {...message} />
       )}
