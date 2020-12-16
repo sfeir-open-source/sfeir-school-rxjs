9c630ddbf7b3cedcf74c14295133b414b4848b38
jefbinomed <jean.francois.garreau@gmail.com>
2020-12-16 17:49:30 +0100

    exercices: remove react code -> vanilla App is the same

--- exercises/react-chat/src/constants.js
+++
@@ -1,3 +0,0 @@
-import * as io from 'socket.io-client'
-
-export const SOCKET = io('http://localhost:8080') // replace this url by the ngrok url if you want to join existing chat
--- exercises/react-chat/src/helpers.js
+++
@@ -1,4 +0,0 @@
-const hourTimeFormat = new Intl.DateTimeFormat('default', {hour24: true, hour: "numeric", minute: "numeric"});
-export const getHourTime = () => {
- return hourTimeFormat.format(new Date())
-}
