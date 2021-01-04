86d570ea1cabe9ff24dcc367676cd0db62bef5b6
Anthony Pena <anthony.pena@outlook.fr>
2021-01-04 16:18:44 +0100

    doc: add anthony speaker slide

--- docs/scripts/slides.js
+++ docs/scripts/slides.js
@@ -3,6 +3,7 @@ function schoolSlides() {
     '00-school/00-TITLE.md',
     '00-school/speaker-jef.md',
     '00-school/speaker-wolf.md',
+    '00-school/speaker-anthony.md'
   ];
 }
 
@@ -13,7 +14,7 @@ function arraySequencesSlides() {
     `${directory}/01-intro.md`,
     `${directory}/02-exercice-intro.md`,
     `${directory}/03-array-vs-generator.md`,
-    `${directory}/04-exercice-sequence.md`,
+    `${directory}/04-exercice-sequence.md`
   ];
 }
 
@@ -22,7 +23,7 @@ function rxjsObservablesSlides() {
   return [
     `${directory}/00-title.md`,
     `${directory}/01-observable-pattern.md`,
-    `${directory}/02-rxjs-observables.md`,
+    `${directory}/02-rxjs-observables.md`
   ];
 }
 
@@ -34,7 +35,7 @@ function observableSlides() {
     `${directory}/02-subscription.md`,
     `${directory}/03-exercice-tchat-1.md`,
     `${directory}/04-hot-cold.md`,
-    `${directory}/05-exercice-tchat-2.md`,
+    `${directory}/05-exercice-tchat-2.md`
   ];
 }
 
@@ -46,7 +47,7 @@ function operatorsSlides() {
     `${directory}/02-exercice-tchat-3.md`,
     `${directory}/03-rxjs-operators.md`,
     `${directory}/04-main-operators.md`,
-    `${directory}/05-exercice-tchat-4.md`,
+    `${directory}/05-exercice-tchat-4.md`
   ];
 }
 
@@ -58,7 +59,7 @@ function subjectSlides() {
     `${directory}/02-subjects.md`,
     `${directory}/03-sharables.md`,
     `${directory}/04-side-effects.md`,
-    `${directory}/05-time.md`,
+    `${directory}/05-time.md`
   ];
 }
 function testsSlides() {
@@ -66,7 +67,7 @@ function testsSlides() {
   return [
     `${directory}/00-title.md`,
     `${directory}/01-marbles.md`,
-    `${directory}/02-scheduler.md`,
+    `${directory}/02-scheduler.md`
   ];
 }
 
@@ -79,8 +80,8 @@ function formation() {
     ...observableSlides(),
     ...operatorsSlides(),
     ...subjectSlides(),
-    ...testsSlides(),
-  ].map((slidePath) => {
+    ...testsSlides()
+  ].map(slidePath => {
     return { path: slidePath };
   });
 }
