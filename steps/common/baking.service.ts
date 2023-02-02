102e5d7479012381560e6e8c735f900286946586
Anthony PENA <pena.a@sfeir.com>
2023-02-02 23:15:03 +0100

    feat(lab): reduce pie pastry interval and compote baking time

--- steps/common/baking.service.ts
+++ steps/common/baking.service.ts
@@ -6,7 +6,7 @@ class BakingServiceImpl {
     if (apples.length !== 4) {
       return throwError(() => new Error('Cannot make compote without 4 apples'));
     }
-    return of<Compote>({ _type: 'Compote' }).pipe(delay(5_000));
+    return of<Compote>({ _type: 'Compote' }).pipe(delay(2_000));
   }
 
   public bakeApplePie(piePastryInPlate: PiePlate, compote: Compote, appleSlices: AppleSlice[]): Observable<ApplePie> {
