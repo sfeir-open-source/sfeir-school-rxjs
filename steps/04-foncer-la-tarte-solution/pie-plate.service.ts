33a53fd5268477a50c845052fb8a03c8dff0e1c6
Lou-Theo <28760135+lou-theo@users.noreply.github.com>
2023-04-24 21:37:25 +0200

    feat: harmonize service usage
    Move PiePlateService in common folder

--- steps/04-foncer-la-tarte-solution/pie-plate.service.ts
+++ steps/04-foncer-la-tarte-solution/pie-plate.service.ts
@@ -1,10 +1,5 @@
-import { interval, map } from 'rxjs';
-import { PiePlate } from '../common/models';
+import { PiePlateService } from '../common';
 
-class PiePlateServiceImpl {
-  getPiePlate() {
-    return interval(1_000).pipe(map((): PiePlate => ({ _type: 'PiePlate' })));
-  }
-}
-
-export const PiePlateService = new PiePlateServiceImpl();
+/**
+ * See correction in {@link PiePlateService}[steps/common/pie-plate.service.ts]
+ */
