5001d30060863463414ca9f96c11e1fde96347ad
Anthony Pena <anthony.pena@outlook.fr>
2025-12-05 17:50:06 +0100

    chore(docs): update theme to v4

---
+++ docs/web_modules/sfeir-school-theme/dist/sfeir-school-theme.d.ts
@@ -0,0 +1,16 @@
+import { Reveal as Reveal_2 } from '@talk-control/talk-control-revealjs-extensions';
+
+export { Reveal_2 as Reveal }
+
+export declare const SfeirThemeInitializer: {
+    /**
+     * @param {() => Array.<string>} slidesFactory
+     */
+    init(slidesFactory: (showType?: string) => SlidePath[], knowStyles?: string[]): Promise<void>;
+};
+
+declare interface SlidePath {
+    path: string;
+}
+
+export { }
